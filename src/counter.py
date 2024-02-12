e810862eb2177455b1cae7b8eac42a23f2d6b5df
39otsu <mccodi11@gmail.com>
2024-02-12 01:01:00 -0800

    fix E302 expected 2 blank lines, found 1

--- src/counter.py
+++ src/counter.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 
 COUNTERS = {}
 
+
 @app.route('/counters/<name>', methods=['POST'])
 def create_counter(name):
     """Create a counter"""
@@ -15,6 +16,7 @@ def create_counter(name):
     COUNTERS[name] = 0
     return {name: COUNTERS[name]}, status.HTTP_201_CREATED
 
+
 @app.route('/counters/<name>', methods=['PUT'])
 def update_counter(name):
     """update a counter"""
@@ -24,6 +26,7 @@ def update_counter(name):
         return {"Message": f"Counter {name} does not exist."}, status.HTTP_404_NOT_FOUND
     COUNTERS[name] = COUNTERS[name] + 1
     return {name: COUNTERS[name]}, status.HTTP_200_OK
+
 
 @app.route('/counters/<name>', methods=['DELETE'])
 def delete_counter(name):
