0de20f0a7e699df6da26982cf74f00425c43c89c
esquek1 <61226686+esquek1@users.noreply.github.com>
2024-02-12 22:36:43 -0800

    Update counter.py

--- src/counter.py
+++ src/counter.py
@@ -47,6 +47,8 @@ def delete_counter(name):
     app.logger.info(f"Request to delete counter: {name}")
     global COUNTERS
     # Delete the count and 204_NO_CONTENT
-    # if name in COUNTERS:
-    del COUNTERS[name]
-    return {"Message": f"Counter {name} has been deleted"},  status.HTTP_204_NO_CONTENT
+    if name in COUNTERS:
+        del COUNTERS[name]
+        return {"Message": f"Counter {name} has been deleted"},  status.HTTP_204_NO_CONTENT
+    # Trying to delete a counter that does not exist
+    return {"Message": f"Counter {name} does not exists"}, status.HTTP_404_NOT_FOUND
